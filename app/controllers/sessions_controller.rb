5d89da93ca92d8332077c72084e06e4264921bf7
Amereen Hanvel <amereenhanvel786@gmail.com>
2023-09-01 21:16:56 -0400

    commit tree e081d42570d34097b76fe189b5ff48d329d9bcdf

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,12 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      render json: user, status: :ok
+    else
+      render json: { error: 'Invalid email or password' }, status: :unauthorized
+    end
+  end
+end
